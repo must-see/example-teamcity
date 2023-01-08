5d3c0ce23627c2f8641e9dd62cb05777c9018413
must-see <97622349+must-see@users.noreply.github.com>
2023-01-08 20:02:04 +0000

    test

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -16,5 +16,4 @@ public class Welcomer{
 	public String sayHunter() {
             return "hunter";
     }
-
 }
--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -16,12 +16,7 @@ public class WelcomerTest {
 	@Test
 	public void welcomerSaysFarewell() {
 		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
+	}	
 	@Test
 	public void welcomerSaysSilver(){
 		assertThat(welcomer.sayNeedGold(), containsString("gold"));
